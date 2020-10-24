c6cd49e52fa3664e078d7e17cd9039d0f9558607
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2020-10-25 01:06:24 +0530

    code

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
